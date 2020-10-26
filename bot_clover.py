c786cd54de3356b21ea2f9b43aced3aea23f3990
AGolz <aloyna.golz@gmail.com>
2020-10-26 22:10:46 +0300

    _c

--- bot_clover.py
+++ bot_clover.py
@@ -1,7 +1,9 @@
-import telebot
-import cherrypy
-import config
 import os
+import config
+
+import cherrypy
+import telebot
+
 
 WEBHOOK_HOST = 'botclover.herokuapp.com'
 WEBHOOK_PORT = 8443
@@ -10,7 +12,7 @@ WEBHOOK_LISTEN = '0.0.0.0'
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % (config.token)
 
-BOT_MAILBOX = 'https://botclover.herokuapp.com'
+BOT_MAILBOX = 'http://193.33.179.204','http://173.192.101.194','http://178.62.47.76'
 
 print('connecting_tg')
 
@@ -18,18 +20,28 @@ bot = telebot.TeleBot(config.token)
 
 print('bot started')
 
+
+
 class WebhookServer(object):
-    @cherrypy.expose
-    def WEBHOOK_HOST(self):
-        if 'content-length' in cherrypy.request.headers and 'content-type' in cherrypy.request.headers and  cherrypy.request.headers['content-type'] == 'application/json':
-            length = int(cherrypy.request.headers['content-length'])
-            json_string = cherrypy.request.body.read(length).decode("utf-8")
-            requests.post(BOT_MAILBOX, data=json_string)
-            return ''
-        
-        else:
-            raise cherrypy.HTTPError(403)
-            
+@cherrypy.expose
+def BOT_MAILBOX(self):
+       if 'content-length' in cherrypy.request.headers and 'content-type' in cherrypy.request.headers and cherrypy.request.headers['content-type'] == 'application/json':
+           length = int(cherrypy.request.headers['content-length'])
+           json_string = cherrypy.request.body.read(length).decode("utf-8")
+           update = telebot.types.Update.de_json(BOT_MAILBOX, data=json_string)
+
+           global jsonObj
+           jsonObj = json.loads(json_string)
+           
+           print(jsonObj)
+           bot.process_new_updates([update])
+           return ''
+       
+       else:
+           raise cherrypy.HTTPError(403)
+       
+       
+    
 
 
 
@@ -48,5 +60,5 @@ cherrypy.config.update({
     'server.socket_port': WEBHOOK_PORT
 })
 
-cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
+cherrypy.quickstart(WebhookServer(), BOT_MAILBOX, {'/': {}})
 
