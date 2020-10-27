48c1a5417c46ae2a08518b92b35fbf66d1dcbbde
AGolz <aloyna.golz@gmail.com>
2020-10-27 22:20:39 +0300

    webhook

--- bot_clover.py
+++ bot_clover.py
@@ -1,18 +1,50 @@
-
 import config
+import telebot
+from aiohttp import web
+import ssl
+
+WEBHOOK_LISTEN = "0.0.0.0"
+WEBHOOK_PORT = 8443
+
+WEBHOOK_SSL_CERT = 'https://botclover.herokuapp.com'
+
+API_TOKEN = config.token
+bot = telebot.TeleBot(API_TOKEN)
+
+app = web.Application()
+
+async def handle(request):
+    if request.match_info.get("token") == bot.token:
+        request_body_dict = await request.json()
+        update = telebot.types.Update.de_json(request_body_dict)
+        bot.process_new_updates([update])
+        return web.Response()
+    else:
+        return web.Response(status=403)
+
+app.router.add_post("/{token}/", handle)
+
+help_string = []
+help_string.append("*Some bot* - just a bot.\n\n")
+help_string.append("/start - greetings\n")
+help_string.append("/help - shows this help")
 
 
-import requests
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "привет")
 
-url = 'https://api.telegram.org/bot'+config.token+'/getUpdates'
+@bot.message_handler(commands=["help"])
+def send_help(message):
+    bot.send_message(message.chat.id, "".join(help_string), parse_mode="Markdown")
 
 
-def get_updates_json(request):  
-    response = requests.get(request + 'getUpdates')
-    return response.json()
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain(WEBHOOK_SSL_CERT)
 
-
-def last_update(data):  
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
+web.run_app(
+    app,
+    host=WEBHOOK_LISTEN,
+    port=WEBHOOK_PORT,
+    ssl_context=context,
+)
