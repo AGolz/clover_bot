2942434097f026c74a8a2d3cd75263339acb695f
AGolz <aloyna.golz@gmail.com>
2020-10-27 22:31:34 +0300

    webhook

--- bot_clover.py
+++ bot_clover.py
@@ -3,12 +3,15 @@ import telebot
 from aiohttp import web
 import ssl
 
+
+WEBHOOK_HOST = 'botclover.herokuapp.com'
 WEBHOOK_LISTEN = "0.0.0.0"
 WEBHOOK_PORT = 8443
 
-WEBHOOK_SSL_CERT = 'https://botclover.herokuapp.com'
-
 API_TOKEN = config.token
+WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
+
 bot = telebot.TeleBot(API_TOKEN)
 
 app = web.Application()
@@ -40,7 +43,7 @@ def send_help(message):
 
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-context.load_cert_chain(WEBHOOK_SSL_CERT)
+context.load_cert_chain()
 
 web.run_app(
     app,
