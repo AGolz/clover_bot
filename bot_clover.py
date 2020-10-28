1e6266d00eaf8c2beaac84bdbf4a175835b0f82d
AGolz <aloyna.golz@gmail.com>
2020-10-28 05:21:54 +0300

    webhook

--- bot_clover.py
+++ bot_clover.py
@@ -44,3 +44,57 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+    
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+
+import config 
+
+PORT = int(os.environ.get('PORT', '8443'))
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+TOKEN = config.token
+
+
+def start(update, context):
+    update.message.reply_text('привет')
+
+def help(update, context):
+    update.message.reply_text('на помощь')
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    
+    dp = updater.dispatcher
+    
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    
+    dp.add_error_handler(error)
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN)
+    updater.bot.set_webhook('https://botclover.herokuapp.com/' + TOKEN)
+    updater.idle()
+
+
+   
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
