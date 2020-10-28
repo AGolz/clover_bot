b051b2208baf50eba57ee65e80eaffab479b240d
AGolz <aloyna.golz@gmail.com>
2020-10-28 04:04:47 +0300

    webhook

--- bot_clover.py
+++ bot_clover.py
@@ -1,10 +1,7 @@
-import logging
-import ssl
-import time
-from telegram import Update
-from telegram.ext import Updater
 
-from aiohttp import web
+from telegram import Update
+from telegram.ext import Updater, dispatcher
+
 
 import telebot
 import config
@@ -18,32 +15,14 @@ updater = Updater(TOKEN)
 bot = telebot.TeleBot(TOKEN)
 
 
-bot = telebot.TeleBot(TOKEN)
-
-app = web.Application()
-
-async def handle(request):
-    print("WEBHOOK")
-    print(request)
-    if request.match_info.get("token") == bot.token:
-        request_body_dict = await request.json()
-        update = telebot.types.Update.de_json(request_body_dict)
-        bot.process_new_updates([update])
-        return web.Response()
-    else:
-        return web.Response(status=403)
-
-app.router.add_post("/{token}/", handle)
+def webhook(update):
+    dispatcher.process_update(update)
 
 
 
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, 'привет')
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, message.text)
 
 
 updater.start_webhook(listen="0.0.0.0",
