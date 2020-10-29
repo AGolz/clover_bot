663a9bb7bc285006ea1c4f883dd2a727fb7ef2af
AGolz <aloyna.golz@gmail.com>
2020-10-29 09:45:39 +0300

    test

--- bot_clover.py
+++ bot_clover.py
@@ -6,47 +6,32 @@ import cherrypy
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
 
-import cost 
-       
+import cost
+
+
 class Website(object):
     @cherrypy.expose
     def index(self):
-        return """<H1>Hi! Look for me in telegram @Padraig_clover_bot ;)</H1>"""  
-    
-        
+        return """<H1>Hi! Look for me in telegram @Padraig_clover_bot ;)</H1>"""
+
+
+@cherrypy.tools.json_in()
+def POST():
+    update = cherrypy.request.json
+    update = telegram.Update.de_json(update, updater)
+    dp.process_update(update)
+
+
 def dispatch_error(error, update):
     cherrypy.log('Error occurred - {}'.format(error))
 
-def start(updater, update, context):
+
+def start(update, context):
     update.effective_message.reply_text('Ку')
 
-def echo(updater, update, context):
+
+def echo(update, context):
     update.effective_message.reply_text(update.effective_message.text)
-
-    
-
-def main():
-    exposed = True
-    
-    @cherrypy.tools.json_in()
-    def POST(main):
-        update = cherrypy.request.json
-        update = telegram.Update.de_json(update, bot)
-        dp.process_update(update) 
-    
-    try:
-        bot.setWebhook('https://{}.herokuapp.com/{}'.format(NAME,TOKEN))
-    except:
-        raise RuntimeError('Failed to set the webhook')
-    
-    updater = Updater(TOKEN, use_context=True)
-    update_queue = Queue()
-    dp = Dispatcher(updater, update_queue)
-    
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-    
-    dp.add_error_handler(dispatch_error)
 
 
 TOKEN = cost.token
@@ -54,12 +39,31 @@ NAME = cost.nameapp
 PORT = os.environ.get('PORT', 8443)
 bot = telegram.Bot(TOKEN)
 
+try:
+    bot.setWebhook('https://{}.herokuapp.com/{}'.format(NAME, TOKEN))
+except:
+    raise RuntimeError('Failed to set the webhook')
+
+updater = Updater(TOKEN, use_context=True)
+update_queue = Queue()
+
+dp = Dispatcher(updater, update_queue)
+
+dp.add_handler(CommandHandler('start', start))
+dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+dp.add_error_handler(dispatch_error)
+
+
+def main():
+    exposed = True
+
+
 if __name__ == '__main__':
-    
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
     logger = logging.getLogger(__name__)
-    
+
     cherrypy.config.update({'server.socket_host': '0.0.0.0', })
     cherrypy.config.update({'server.socket_port': int(PORT), })
     cherrypy.tree.mount(Website(), "/")
