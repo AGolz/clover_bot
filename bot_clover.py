411fe8ead69ecabe4edec80ce47e996fcb578a88
AGolz <aloyna.golz@gmail.com>
2020-10-29 14:51:35 +0300

    without_cherry

--- bot_clover.py
+++ bot_clover.py
@@ -34,7 +34,7 @@ def  main ():
     
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(MessageHandler(Filters.text, echo))
-    dp.add_error_handler(error)
+    dp.add_error_handler(dispatch_error)
 
     
     updater.start_webhook(listen="0.0.0.0",
