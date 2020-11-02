d9908e1f99ce8fe1f641039e53cad1f516afef00
AGolz <aloyna.golz@gmail.com>
2020-11-02 14:07:21 +0300

    context

--- bot_clover.py
+++ bot_clover.py
@@ -49,7 +49,7 @@ class Root_bot(object):
     def _echo(update: Update, context: CallbackContext):
         update.effective_message.reply_text(update.effective_message.text)
         
-    def _error_callback(context, update):
+    def _error_callback(self, context, update):
         cherrypy.log("Error occurred - {}".format(context.error))
         
     
