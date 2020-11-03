d53b09399fe84c0e02d6762681422d97a81cce4e
AGolz <aloyna.golz@gmail.com>
2020-11-03 15:55:14 +0300

    echo

--- bot_clover.py
+++ bot_clover.py
@@ -1,63 +1,37 @@
 import logging
-import os
 
-from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler, CallbackContext
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 import config 
 
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
 logger = logging.getLogger(__name__)
 
-   
-    
-def caps(update, context):
-    text_caps = ' '.join(context.args).upper()
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
-    
-    
-def inline_caps(update, context):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    context.bot.answer_inline_query(update.inline_query.id, results)
-    
-    
+
 def start(update: Update, context: CallbackContext):
-    update.effective_message.reply_text('Ку!')
-    
-    
-def echo(update: Update, context: CallbackContext): 
+    update.message.reply_text('Ку')
+
+
+def echo(update: Update, context: CallbackContext):
     update.message.reply_text(update.message.text)
 
-    
-    
-       
+
 def main():
-    updater = Updater(token=config.token, use_context=True)
+    updater = Updater(config.token, use_context=True)
     dispatcher = updater.dispatcher
-    
-    dispatcher.add_handler(CommandHandler('caps', caps))
-    dispatcher.add_handler(InlineQueryHandler(inline_caps))
     
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
     
-
     updater.start_polling()
-    
+    updater.idle()
+
 
 if __name__ == '__main__':
     main()
-    
     
