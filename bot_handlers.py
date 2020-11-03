7336a3a5a5e778e1d9716241710b00e0cc98db39
AGolz <aloyna.golz@gmail.com>
2020-11-04 00:28:46 +0300

    bot_handlers.py

--- bot_handlers.py
+++ bot_handlers.py
@@ -3,10 +3,8 @@ from telegram import Update, update
 from telegram.ext import CallbackQueryHandler, CallbackContext
 
 
-
-
-def start(bot, update : Update, context : CallbackContext):
+def start(Manage_bot, update : Update, context : CallbackContext):
         update.effective_message.reply_text("Ку")
             
-def echo(bot, update : Update, context : CallbackContext):
+def echo(Manage_bot, update : Update, context : CallbackContext):
         update.effective_message.reply_text(update.message.text)
