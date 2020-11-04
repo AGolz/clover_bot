1625b235a9e83700deaa1fdf3846fa33145a66f3
AGolz <aloyna.golz@gmail.com>
2020-11-04 04:12:44 +0300

    chat_adm

--- chat_adm.py
+++ chat_adm.py
@@ -1,65 +1,25 @@
 import config
-import os
-import telebot
-import time
-
-bot = telebot.TeleBot(config.token)
+from telegram import Update, message
+from telegram.ext import CallbackQueryHandler, CallbackContext
 
 
-def check_admin(message):
-    if message.chat.id != config.admin:
-        bot.send_message(message.chat.id, text='Вы не админ!')
+class AdminComm(object):
     
-
-@bot.message_handler(commands=['add_photo']) 
-def add_photo(message):
-    if check_admin(message): return
+    def check_admin(self, update : Update, context : CallbackContext):
+        user_id = str(update.effective_user.id)
+        user_name = update.effective_user.username
         
-    else:
-        bot.send_message(config.admin, text='Кидай photo')
-        @bot.message_handler(content_types=['photo'])
+        if user_id != config.admin:
+            update.effective_message.reply_text('Access denied for {}.'.format(user_name))
+     
+            
+    @staticmethod
+    def test(update : Update, context : CallbackContext):
+        if check_admin(): return
         
-        def handle_docs_photo(message):
-            photo_id = message.photo[-1].file_id
-            file_info = bot.get_file(photo_id)
-            bot.send_message(config.admin, file_info.file_id, reply_to_message_id=file_info.file_id)
-            print(message.photo[0:])
-           
-    time.sleep(3)
-    
-    
-@bot.message_handler(commands=['add_audio']) 
-def add_audio(message):
-    if check_admin(message): return
-        
-    else:
-        bot.send_message(config.admin, text='Кидай audio')
-        @bot.message_handler(content_types=['audio'])
-
-        def handle_docs_audio(message):
-            audio_id = message.audio.file_id
-            file_info = bot.get_file(audio_id)
-            bot.send_message(config.admin, file_info.file_id, reply_to_message_id=file_info.file_id)
-        
-             
-    time.sleep(3)
-    
-@bot.message_handler(commands=['add_doc']) 
-def add_doc(message):
-    if check_admin(message): return
-        
-    else:
-        bot.send_message(config.admin, text='Кидай файл')
-        @bot.message_handler(content_types=['document'])
-
-        def handle_docs_document(message):
-            document_id = message.document.file_id
-            file_info = bot.get_file(document_id)
-            bot.send_message(config.admin, file_info.file_id, reply_to_message_id=file_info.file_id)
-           
-    time.sleep(3)
-
-
-
-if __name__ == '__main__':
-    bot.infinity_polling()
+        else:
+            update.effective_message.reply_text('Кидай фото')
+            if message.photo:
+                photo_id = update.message.photo[-1].get_file()
+                update.effective_message.reply_text(photo_id)
+          
