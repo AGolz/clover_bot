68fd58574db58ef72069fac018f02c2bc3d73bc1
AGolz <aloyna.golz@gmail.com>
2020-11-04 04:20:34 +0300

    chat_adm

--- chat_adm.py
+++ chat_adm.py
@@ -3,23 +3,23 @@ from telegram import Update, message
 from telegram.ext import CallbackQueryHandler, CallbackContext
 
 
-class AdminComm(object):
+
     
-    def check_admin(self, update : Update, context : CallbackContext):
-        user_id = str(update.effective_user.id)
-        user_name = update.effective_user.username
-        
-        if user_id != config.admin:
-            update.effective_message.reply_text('Access denied for {}.'.format(user_name))
+    
+def check_admin(update : Update, context : CallbackContext):
+    user_id = str(update.effective_user.id)
+    user_name = update.effective_user.username
+    
+    if user_id != config.admin:
+        update.effective_message.reply_text('Access denied for {}.'.format(user_name))
      
             
-    @staticmethod
-    def test(update : Update, context : CallbackContext):
-        if check_admin(): return
-        
-        else:
-            update.effective_message.reply_text('Кидай фото')
-            if message.photo:
-                photo_id = update.message.photo[-1].get_file()
-                update.effective_message.reply_text(photo_id)
+@staticmethod
+def test(update : Update, context : CallbackContext):
+    if check_admin(): return
+    else:
+        update.effective_message.reply_text('Кидай фото')
+        if message.photo:
+            photo_id = update.message.photo[-1].get_file()
+            update.effective_message.reply_text(photo_id)
           
