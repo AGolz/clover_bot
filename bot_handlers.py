943905a26946bf25a977e1f8b5f9791ea45bd10e
AGolz <aloyna.golz@gmail.com>
2020-11-06 00:12:42 +0300

    adm_comm

--- bot_handlers.py
+++ bot_handlers.py
@@ -3,15 +3,12 @@ import time
 from telegram import Update
 from telegram.ext import CallbackQueryHandler, CallbackContext
 
-
 class GenericComm(object):
    
     def start(update : Update, context : CallbackContext):
         update.effective_message.reply_text("Ку")
         time.sleep(3)
-        
-    
-            
+                  
     def echo(update : Update, context : CallbackContext):
         update.effective_message.reply_text(update.message.text)
         time.sleep(3)
