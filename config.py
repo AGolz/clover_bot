30ce2ccc1b925a5c94292f2cfc8baaa0c42eae32
AGolz <aloyna.golz@gmail.com>
2020-11-15 10:14:25 +0300

    addSQL

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 
 token = os.environ['TOKEN']
 db_url = os.environ['DATABASE_URL']
-column_id = None
 admin = 380448331
 nameapp = 'botclover'
+column_id = None
 PHOTO = 0
 AUDIO = 0
 DOCS = 0
