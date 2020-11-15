2636bea66a601ca7d3c2ddb753f8f1deb45955b6
AGolz <aloyna.golz@gmail.com>
2020-11-15 10:02:18 +0300

    addSQL

--- bot_handlers.py
+++ bot_handlers.py
@@ -15,7 +15,6 @@ class GenericComm(object):
         config.column_id = 101
         psql = PostgreSQL()
         file_id = psql.extract_id()
-        print(file_id)
         context.bot.send_audio(chat_id=update.effective_chat.id, audio=file_id)
         time.sleep(3)
         
--- postgre_sql.py
+++ postgre_sql.py
@@ -11,8 +11,7 @@ class PostgreSQL:
             with conn, conn.cursor() as cur: 
                 cur.execute("SELECT * FROM admin_file WHERE id = %d" % (config.column_id))
                 file_id = cur.fetchone()[0].strip()
-                print(file_id)
-        return file_id
+            return file_id
                     
                 
             
