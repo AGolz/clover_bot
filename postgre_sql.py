749276f1da3263044407bf8d3f9df26f55bcc907
AGolz <aloyna.golz@gmail.com>
2020-11-15 08:12:47 +0300

    addSQL

--- postgre_sql.py
+++ postgre_sql.py
@@ -9,7 +9,7 @@ class PostgreSQL:
         with psycopg2.connect(config.db_url) as conn:
             print('Database opened')
             with conn, conn.cursor() as cur: 
-                cur.execute('SELECT * FROM admin_file WHERE id = %s',(config.column_id))
+                cur.execute("SELECT * FROM admin_file WHERE id = %d" % (config.column_id))
                 db_row = cur.fetchone()
                 for row in db_row:
                     file_id = row['file_id']
